3b11b58d9d3f8197197ed21abc510c3d5713affb
t510599 <t510599@gmail.com>
2018-07-05 00:26:57 +0800

    update script

--- db_export.sh
+++ db_export.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mongoexport -d 2018-cscamp -c register --type=csv -o register.csv -f fullname,gender,size,school,phone,parentName,transport,email,partner,id,birthday,emergencyContact,parentalConsent,agree,message,time
+mongoexport -d 2018-cscamp -c register --type=csv -o register.csv -f fullname,gender,size,school,phone,parentName,transport,email,id,birthday,emergencyContact,parentalConsent,message,time
 zip -r -0 2018_cscamp_registration.zip upload/ register.csv
